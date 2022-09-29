1c4b6c73032730dc974e0f8a3c45a61ab75cb53a
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-09-29 03:08:08 +0200

    feat: add gg font montserrat & style error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,44 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: ${colors.primary};
+`;
+
+const ErrorTitle = styled.h1`
+  font-size: 288px;
+  font-weight: 700;
+  line-height: 411px;
+  margin-bottom: 66px;
+`;
+
+const ErrorContent = styled.h3`
+  font-size: 36px;
+  font-weight: 500;
+  line-height: 51px;
+  margin-bottom: 182px;
+`;
+
+const HomeLink = styled(Link)`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 26px;
+  margin-bottom: 159px;
+`;
+
 function Error() {
   return (
-    <div>
-      <h1>404 page</h1>
-    </div>
-  )
+    <ErrorContainer>
+      <ErrorTitle>404</ErrorTitle>
+      <ErrorContent>Oups! La page que vous demandez n'existe pas.</ErrorContent>
+
+      <HomeLink to="/">Retourner sur la page d’accueil</HomeLink>
+    </ErrorContainer>
+  );
 }
 
-export default Error
+export default Error;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import styled, { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
+  }
+  body {
     font-family: 'Montserrat', sans-serif;
   }
 `;
